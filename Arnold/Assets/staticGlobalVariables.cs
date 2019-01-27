843165f967545e208d19986e22c47585d8044cad
Jakub Bober <jakub.bober18@imperial.ac.uk>
2019-01-27 10:49:55 +0000

    Arnold deleted

--- Arnold/Assets/staticGlobalVariables.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-public static class staticGlobalVariables
-{
-    public static string next_scene = "Login";
-    public static string roomName;
-}
--- Arnold/Library/Collab/Base/Assets/staticGlobalVariables.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-public static class staticGlobalVariables
-{
-    public static int next_scene = 0;
-}
